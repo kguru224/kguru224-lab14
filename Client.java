956965ce5a7c4f98a338440f375f54497fca5322
Kiran Guru <k.guru@gwmail.gwu.edu>
2025-11-12 12:11:32 -0500

    Implemented basic skeleton of Client.java

---
+++ Client.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+
+    private Socket socket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public Client(String host, int port) {
+
+    }
+
+    public boolean handshake() {
+
+    }
+
+    public String requestFactorization(int num) {
+
+    }
+
+    public void close() {
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
